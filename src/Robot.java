2ee49ce68ed7d6e07e01d2250b77cf8499bb3273
Gitravail <raphael.tournafond@gmail.com>
2019-02-05 15:25:12 +0100

    Create Robot.java

---
+++ src/Robot.java
@@ -0,0 +1,78 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+public class Robot {
+    public Cube cubeTenu;
+
+    public Bool créerCube(TailleCube Taille, String Couleur) {
+//begin of modifiable zone(JavaCode)......C/cbcc23a3-773a-4325-ab58-4c31de565109
+
+//end of modifiable zone(JavaCode)........E/cbcc23a3-773a-4325-ab58-4c31de565109
+//begin of modifiable zone(JavaReturned)..C/cbcc23a3-773a-4325-ab58-4c31de565109
+
+//end of modifiable zone(JavaReturned)....E/cbcc23a3-773a-4325-ab58-4c31de565109
+    }
+
+    public Bool pinceLibre() {
+//begin of modifiable zone(JavaCode)......C/2c3c0591-c2b5-47c9-9491-bef8fbf206c1
+
+//end of modifiable zone(JavaCode)........E/2c3c0591-c2b5-47c9-9491-bef8fbf206c1
+//begin of modifiable zone(JavaReturned)..C/2c3c0591-c2b5-47c9-9491-bef8fbf206c1
+
+//end of modifiable zone(JavaReturned)....E/2c3c0591-c2b5-47c9-9491-bef8fbf206c1
+    }
+
+    public Bool tenirCube(Cube CubeATenir) {
+//begin of modifiable zone(JavaCode)......C/84153ce6-df5c-44c0-a0f7-eb19bb8943d9
+
+//end of modifiable zone(JavaCode)........E/84153ce6-df5c-44c0-a0f7-eb19bb8943d9
+//begin of modifiable zone(JavaReturned)..C/84153ce6-df5c-44c0-a0f7-eb19bb8943d9
+
+//end of modifiable zone(JavaReturned)....E/84153ce6-df5c-44c0-a0f7-eb19bb8943d9
+    }
+
+    public Bool détruireCube() {
+//begin of modifiable zone(JavaCode)......C/7100d31b-740d-4a9e-8348-921e0ef3baae
+
+//end of modifiable zone(JavaCode)........E/7100d31b-740d-4a9e-8348-921e0ef3baae
+//begin of modifiable zone(JavaReturned)..C/7100d31b-740d-4a9e-8348-921e0ef3baae
+
+//end of modifiable zone(JavaReturned)....E/7100d31b-740d-4a9e-8348-921e0ef3baae
+    }
+
+    public Bool lacherCube() {
+//begin of modifiable zone(JavaCode)......C/6b3607e5-fe7b-40ba-8e89-55a81087c686
+
+//end of modifiable zone(JavaCode)........E/6b3607e5-fe7b-40ba-8e89-55a81087c686
+//begin of modifiable zone(JavaReturned)..C/6b3607e5-fe7b-40ba-8e89-55a81087c686
+
+//end of modifiable zone(JavaReturned)....E/6b3607e5-fe7b-40ba-8e89-55a81087c686
+    }
+
+    public Bool prendreCube(TailleCube Taille, String Couleur) {
+//begin of modifiable zone(JavaCode)......C/a61de092-67c4-428d-a37d-44f0f364763a
+
+//end of modifiable zone(JavaCode)........E/a61de092-67c4-428d-a37d-44f0f364763a
+//begin of modifiable zone(JavaReturned)..C/a61de092-67c4-428d-a37d-44f0f364763a
+
+//end of modifiable zone(JavaReturned)....E/a61de092-67c4-428d-a37d-44f0f364763a
+    }
+
+    public Bool poserCubeSurCube(TailleCube taille, String couleur) {
+//begin of modifiable zone(JavaCode)......C/7a727d34-b840-4b49-b411-f24a953a4dcc
+
+//end of modifiable zone(JavaCode)........E/7a727d34-b840-4b49-b411-f24a953a4dcc
+//begin of modifiable zone(JavaReturned)..C/7a727d34-b840-4b49-b411-f24a953a4dcc
+
+//end of modifiable zone(JavaReturned)....E/7a727d34-b840-4b49-b411-f24a953a4dcc
+    }
+
+    public Bool poserCubeSurTable() {
+//begin of modifiable zone(JavaCode)......C/279a2ed8-b923-4ca0-96af-4194935815f4
+
+//end of modifiable zone(JavaCode)........E/279a2ed8-b923-4ca0-96af-4194935815f4
+//begin of modifiable zone(JavaReturned)..C/279a2ed8-b923-4ca0-96af-4194935815f4
+
+//end of modifiable zone(JavaReturned)....E/279a2ed8-b923-4ca0-96af-4194935815f4
+    }
+
+}
