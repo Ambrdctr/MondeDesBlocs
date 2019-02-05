3bc03b8d11954a8e7ef7ba96c4cec0b4b8de4b67
Gitravail <raphael.tournafond@gmail.com>
2019-02-05 15:21:32 +0100

    Create Table.java

---
+++ src/Table.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+public class Table {
+    public List<Cube> sommetDePile = new ArrayList<Cube> ();
+
+    public Bool existeSommet(TailleCube Taille, String Couleur) {
+//begin of modifiable zone(JavaCode)......C/ea76f496-0991-4027-870a-082569861639
+
+//end of modifiable zone(JavaCode)........E/ea76f496-0991-4027-870a-082569861639
+//begin of modifiable zone(JavaReturned)..C/ea76f496-0991-4027-870a-082569861639
+
+//end of modifiable zone(JavaReturned)....E/ea76f496-0991-4027-870a-082569861639
+    }
+
+    public Cube retireSommet(TailleCube Taille, String Couleur) {
+//begin of modifiable zone(JavaCode)......C/f3a9ab2f-56bd-49c4-893d-a2656e97466c
+
+//end of modifiable zone(JavaCode)........E/f3a9ab2f-56bd-49c4-893d-a2656e97466c
+//begin of modifiable zone(JavaReturned)..C/f3a9ab2f-56bd-49c4-893d-a2656e97466c
+
+//end of modifiable zone(JavaReturned)....E/f3a9ab2f-56bd-49c4-893d-a2656e97466c
+    }
+
+    public void lacherSommet(Cube cube) {
+//begin of modifiable zone(JavaCode)......C/ed911d91-be21-45eb-9521-8b780178efce
+
+//end of modifiable zone(JavaCode)........E/ed911d91-be21-45eb-9521-8b780178efce
+    }
+
+    public void nouveauSommet(Cube cube) {
+//begin of modifiable zone(JavaCode)......C/93100a01-6b8f-49f7-9c75-4449cfbc1fdd
+
+//end of modifiable zone(JavaCode)........E/93100a01-6b8f-49f7-9c75-4449cfbc1fdd
+    }
+
+    public Bool poserCubeSurCube(TailleCube taille, String couleur, Cube cube) {
+//begin of modifiable zone(JavaCode)......C/cdcdc004-860b-4e1a-a8d3-68d5391e1d10
+
+//end of modifiable zone(JavaCode)........E/cdcdc004-860b-4e1a-a8d3-68d5391e1d10
+//begin of modifiable zone(JavaReturned)..C/cdcdc004-860b-4e1a-a8d3-68d5391e1d10
+
+//end of modifiable zone(JavaReturned)....E/cdcdc004-860b-4e1a-a8d3-68d5391e1d10
+    }
+
+    public Bool poserPossible(TailleCube taille1, TailleCube taille2) {
+//begin of modifiable zone(JavaCode)......C/a9c94c71-968e-420e-a535-fcdb9f63fe9a
+
+//end of modifiable zone(JavaCode)........E/a9c94c71-968e-420e-a535-fcdb9f63fe9a
+//begin of modifiable zone(JavaReturned)..C/a9c94c71-968e-420e-a535-fcdb9f63fe9a
+
+//end of modifiable zone(JavaReturned)....E/a9c94c71-968e-420e-a535-fcdb9f63fe9a
+    }
+
+    public Bool poserCubeSurTable(Cube cube) {
+//begin of modifiable zone(JavaCode)......C/d1441db3-ffd0-41e3-b4e2-4b49b2fe3d8a
+
+//end of modifiable zone(JavaCode)........E/d1441db3-ffd0-41e3-b4e2-4b49b2fe3d8a
+//begin of modifiable zone(JavaReturned)..C/d1441db3-ffd0-41e3-b4e2-4b49b2fe3d8a
+
+//end of modifiable zone(JavaReturned)....E/d1441db3-ffd0-41e3-b4e2-4b49b2fe3d8a
+    }
+
+}
