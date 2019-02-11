297e67e7d757478f2f062a2025ec3959775d67bc
Ambrdctr <33329206+Ambrdctr@users.noreply.github.com>
2019-02-11 18:25:15 +0100

    robot debut

--- src/Monde.java
+++ src/Monde.java
@@ -2,18 +2,18 @@ import in.keyboard.Keyboard;
 
 public class Monde {
 	
-//	public static final int rouge = 31;
-//	public static final int vert = 32;
-//	public static final int jaune = 33;
-//	public static final int bleu = 34;
-//	public static final int violet = 35;
-//	public static final int cyan = 36;
-//	public static final int gris = 37;
-//	public static final int noir = 38;
+	public static final int rouge = 31;
+	public static final int vert = 32;
+	public static final int jaune = 33;
+	public static final int bleu = 34;
+	public static final int violet = 35;
+	public static final int cyan = 36;
+	public static final int gris = 37;
+	public static final int noir = 38;
 	
 	public static void ecrire(Couleur coul, String s) {
-		// System.out.printf("\033[%d;1m%s\033[0m\n", coul.ordinal()+31, s);
-		System.out.printf(s);
+		System.out.printf("\033[%d;1m%s\033[0m\n", coul.ordinal()+31, s);
+		// System.out.printf(s);
 	}
 	
 	public static void main(String[] args) {
@@ -25,7 +25,7 @@ public class Monde {
 		TailleCube tc;
 		do {
 		System.out.println("1 - creer un cube");
-		System.out.println("2 - détruire un cube");
+		System.out.println("2 - dÃ©truire un cube");
 		System.out.println("3 - prendre un cube");
 		System.out.println("4 - fin du monde");
 		System.out.print("Votre choix : ");
@@ -35,13 +35,13 @@ public class Monde {
 		case 1: 
 			System.out.print("Couleur du cube : ");
 			coul = Couleur.getCouleur(Keyboard.getString());
-//			ecrire (coul, "couleur saisie");
+			ecrire (coul, "couleur saisie");
 			System.out.print("Taille (grand/moyen/petit) : ");
 			tc = TailleCube.getTaille(Keyboard.getString());
 			POC3.creerCube(tc, coul);
 			break;
 		case 2:
-			System.out.println("Le cube tenu par le robot va être détruit ");
+			System.out.println("Le cube tenu par le robot va Ãªtre dÃ©truit ");
 			Keyboard.pause();
 			POC3.detruireCube();
 			break;
