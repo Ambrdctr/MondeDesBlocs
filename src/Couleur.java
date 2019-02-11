faac7ffc55301b1d2da7379a584a0b4b7e8e1ade
Gitravail <raphael.tournafond@gmail.com>
2019-02-11 18:44:02 +0100

    modifs couleur

--- src/Couleur.java
+++ src/Couleur.java
@@ -1,31 +1,17 @@
-import java.io.*;
-
 public enum Couleur
 {
- rouge, vert, jaune, bleu, violet, cyan, blanc, noir, reset;
+ rouge, vert, jaune, bleu, violet, cyan, gris, noir;
  
  public static Couleur getCouleur(String coulSaisie) {
 		for (Couleur c:Couleur.values())
 			if (c.name().equalsIgnoreCase(coulSaisie))
 				return c;		
 		return Couleur.rouge;
- }
-
- public static String getANSI(Couleur c) {
-	switch(c) {
-		case rouge: return "\u001B[31m";
-		case vert: return "\u001B[32m";
-		case jaune: return "\u001B[33m";
-		case bleu: return "\u001B[34m";
-		case violet: return "\u001B[35m";
-		case cyan: return "\u001B[36m";
-		case blanc: return "\u001B[37m";
-		case noir: return "\u001B[30m";
-		default:
-			return "\u001B[0m";
 	}
- }
 
+public static void testCouleur() {
+	for (Couleur c:Couleur.values())
+		Monde.ecrire(c,"test couleur");	
+    }
 
 }
-
